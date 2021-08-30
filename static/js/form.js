3b0c1f99abbf66eedfe39847fa553f6dce052423
ashleigh-tesla <84648968+ashleigh-tesla@users.noreply.github.com>
2021-08-30 16:30:40 +0200

    Add

--- static/js/form.js
+++ static/js/form.js
@@ -1,37 +1,43 @@
 function validate() {
-    var name = document.getElementById("name").value;
-    var subject = document.getElementById("subject").value;
-    var phone = document.getElementById("phone").value;
-    var email = document.getElementById("email").value;
-    var message = document.getElementById("message").value;
-    var error_message = document.getElementById("error_message");
+    var name = document.querySelector("#name").value;
+    var subject = document.querySelector("#subject").value;
+    var service = document.querySelector("#service").value;
+    var phone = document.querySelector("#phone").value;
+    var email = document.querySelector("#email").value;
+    var message = document.querySelector("#message").value;
+    var error_message = document.querySelector("#error_message");
 
-    error_message.style.padding = "10px";
+    error_message.style.padding = "15px";
 
     var text;
-    if (name.length < 5) {
-        text = "Please Enter valid Name";
-        error_message.innerHTML = text;
+    if (name.length <= 3) {
+        text = "Please Enter Valid Name";
+        error_message.innerText = text;
         return false;
     }
-    if (subject.length < 10) {
+    if (subject.length <= 1) {
         text = "Please Enter Correct Subject";
-        error_message.innerHTML = text;
+        error_message.innerText = text;
+        return false;
+    }
+    if (service == ' ') {
+        text = "Good Day Sir / Madam \n\nHow Can I Help You? \n\nPlease Enter Thy Required Service";
+        error_message.innerText = text;
         return false;
     }
     if (isNaN(phone) || phone.length != 10) {
-        text = "Please Enter valid Phone Number";
-        error_message.innerHTML = text;
+        text = "Please Enter Valid Phone Number";
+        error_message.innerText = text;
         return false;
     }
     if (email.indexOf("@") == -1 || email.length < 6) {
-        text = "Please Enter valid Email";
-        error_message.innerHTML = text;
+        text = "Please Enter Valid Email";
+        error_message.innerText = text;
         return false;
     }
-    if (message.length <= 140) {
-        text = "Please Enter More Than 140 Characters";
-        error_message.innerHTML = text;
+    if (message.length <= 70) {
+        text = "Please Enter More Than 70 Characters";
+        error_message.innerText = text;
         return false;
     }
     alert("Form Submitted Successfully!");
